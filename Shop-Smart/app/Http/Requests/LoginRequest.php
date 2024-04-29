8c409eeb2377bbabd30d11b45e8a05c910a90605
ZeyadEhab1 <ziadehabosman@gmail.com>
2024-04-30 01:47:49 +0300

    add eixist check to the login request

--- Shop-Smart/app/Http/Requests/LoginRequest.php
+++ Shop-Smart/app/Http/Requests/LoginRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class LoginRequest extends FormRequest
 {
@@ -22,7 +23,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'email' =>['email','required'],
+            'email' =>['email','required',Rule::exists('users', 'email')],
             'password' => ['required','string']
         ];
     }
