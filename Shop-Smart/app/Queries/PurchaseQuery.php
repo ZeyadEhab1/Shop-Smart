2fee6f3e056d9737879782aafd28c9d81a32a93a
ZeyadEhab1 <ziadehabosman@gmail.com>
2024-04-29 20:53:10 +0300

    adding sorting and filtring functionalty

---
+++ Shop-Smart/app/Queries/PurchaseQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Queries;
+use Illuminate\Database\Eloquent\Builder;
+
+class PurchaseQuery extends Builder
+{
+    public function byDate(?string $startDate, ?string $endDate): PurchaseQuery|static
+    {
+
+        if ($startDate && $endDate) {
+            return $this->whereBetween('created_at', [$startDate, $endDate]);
+        }
+        return $this;
+    }
+
+    public function sortBy(?string $sortBy, string $sortOrder = 'asc')
+    {
+        if ($sortBy) {
+            return $this->orderBy($sortBy, $sortOrder);
+        }
+        return $this;
+    }
+}
