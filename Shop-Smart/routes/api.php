92940309c7ea3b9034bc33f89817c4b9fdbe6b50
zeyad ehab <107952157+ZeyadEhab1@users.noreply.github.com>
2024-04-29 13:52:45 +0300

    purchases (#4)

--- Shop-Smart/routes/api.php
+++ Shop-Smart/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\LoyaltyWalletController;
+use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
-Route::group (['middleware' => ['auth:sanctum']], function() {
+Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [UserController::class, 'logout']);
     Route::get('/loyalty-wallet', LoyaltyWalletController::class);
-
+    Route::get('/purchases', PurchaseController::class);
 });
