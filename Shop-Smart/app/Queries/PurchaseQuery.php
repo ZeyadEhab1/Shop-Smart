42321a01030419e2fd418a9845fcd9acdfe74968
ZeyadEhab1 <ziadehabosman@gmail.com>
2024-04-30 21:21:19 +0300

    added by amount fileration to the purchase

--- Shop-Smart/app/Queries/PurchaseQuery.php
+++ Shop-Smart/app/Queries/PurchaseQuery.php
@@ -14,6 +14,7 @@ class PurchaseQuery extends Builder
         }
         return $this;
     }
+
     public function byAmount(?float $minAmount, ?float $maxAmount): PurchaseQuery
     {
         if ($minAmount !== null && $maxAmount !== null) {
@@ -25,6 +26,7 @@ class PurchaseQuery extends Builder
         }
         return $this;
     }
+
     public function sortBy(?string $sortBy, string $sortOrder = 'asc')
     {
         if ($sortBy) {
